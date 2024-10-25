0bc2b2f56c1ae5cd3e2f01cee581141c29db3441
LinhHoangLuu <luuhoanglinh.2003@gmail.com>
2024-10-25 16:46:34 +0700

    xóa bảng dự kiến

--- src/routes/qcdkRoute.js
+++ src/routes/qcdkRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const obj = require("../controllers/deleteTableDBController"); // Import hàm xử lý từ controller
 
 // Định nghĩa route POST cho xóa dữ liệu
-router.post("/xoa-qcdk", obj.deleteTableTam); // Chỉ cần truyền hàm, không gọi nó
+router.post("/xoa-qcdk", (req, res) => {
+  obj.deleteTableTam(req, res)
+}); // Chỉ cần truyền hàm, không gọi nó
 
 module.exports = router;
