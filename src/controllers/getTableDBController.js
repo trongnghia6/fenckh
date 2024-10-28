bc9b937745516331bde953b4bc569819a65c5a6b
LinhHoangLuu <luuhoanglinh.2003@gmail.com>
2024-10-28 11:08:48 +0700

    phương đần

--- src/controllers/getTableDBController.js
+++ src/controllers/getTableDBController.js
@@ -9,22 +9,66 @@ const connection = require('../controllers/connectDB'); // Giả định rằng 
 let tableTam = process.env.DB_TABLE_TAM;
 let tableQC = process.env.DB_TABLE_QC;
 
-const getTableQC = async () => {
-  try {
-    const query = `SELECT * FROM ${tableQC}`; // Đổi tên bảng nếu cần
-    const results = await new Promise((resolve, reject) => {
-      connection.query(query, (error, results) => {
-        if (error) {
-          console.error('Lỗi truy vấn cơ sở dữ liệu:', error);
-          return reject(new Error('Không thể truy xuất dữ liệu từ cơ sở dữ liệu.')); // Kết thúc nếu có lỗi
-        }
-        resolve(results); // Trả về dữ liệu
+// const getTableQC = async () => {
+//   try {
+//     const query = `SELECT * FROM ${tableQC}`; // Đổi tên bảng nếu cần
+//     const results = await new Promise((resolve, reject) => {
+//       connection.query(query, (error, results) => {
+//         if (error) {
+//           console.error('Lỗi truy vấn cơ sở dữ liệu:', error);
+//           return reject(new Error('Không thể truy xuất dữ liệu từ cơ sở dữ liệu.')); // Kết thúc nếu có lỗi
+//         }
+//         resolve(results); // Trả về dữ liệu
+//       });
+//     });
+//     return results; // Trả kết quả
+//   } catch (error) {
+//     console.error('Lỗi trong hàm getTableQC:', error);
+//     throw error; // Ném lại lỗi cho caller
+//   }
+// };
+
+
+const getTableQC = async (req, res) => {
+  const { Khoa, Dot, Ki, Nam } = req.body;
+  // console.log(req.body)
+
+  console.log('Lấy dữ liệu bảng tạm Khoa Đợt Kì Năm :', Khoa, Dot, Ki, Nam);
+
+  if (Khoa != 'ALL') {
+    try {
+      const query = `SELECT * FROM ${tableQC} WHERE Khoa = ? AND Dot = ? AND KiHoc = ? AND NamHoc = ?`;
+      const results = await new Promise((resolve, reject) => {
+        connection.query(query, [Khoa, Dot, Ki, Nam], (error, results) => {
+          if (error) {
+            console.error('Lỗi truy vấn cơ sở dữ liệu:', error);
+            return reject(new Error('Không thể truy xuất dữ liệu từ cơ sở dữ liệu.'));
+          }
+          resolve(results);
+        });
       });
-    });
-    return results; // Trả kết quả
-  } catch (error) {
-    console.error('Lỗi trong hàm getTableQC:', error);
-    throw error; // Ném lại lỗi cho caller
+      res.json(results); // Trả về kết quả dưới dạng JSON
+    } catch (error) {
+      console.error('Lỗi trong hàm getTableTam:', error);
+      res.status(500).json({ message: 'Không thể truy xuất dữ liệu từ cơ sở dữ liệu.' });
+    }
+  } else {
+    try {
+      const query = `SELECT * FROM ${tableQC} WHERE Dot = ? AND KiHoc = ? AND NamHoc = ?`;
+      const results = await new Promise((resolve, reject) => {
+        connection.query(query, [Dot, Ki, Nam], (error, results) => {
+          if (error) {
+            console.error('Lỗi truy vấn cơ sở dữ liệu:', error);
+            return reject(new Error('Không thể truy xuất dữ liệu từ cơ sở dữ liệu.'));
+          }
+          resolve(results);
+        });
+      });
+      res.json(results); // Trả về kết quả dưới dạng JSON
+    } catch (error) {
+      console.error('Lỗi trong hàm getTableTam:', error);
+      res.status(500).json({ message: 'Không thể truy xuất dữ liệu từ cơ sở dữ liệu.' });
+    }
   }
 };
 
